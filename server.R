217386559a67825da1eeb63aef83d87fc5967acd
nothk <ippo@163.com>
2019-07-24 16:17:20 +0800

    Update server.R

--- server.R
+++ server.R
@@ -15,7 +15,7 @@ if (!file.exists("synset.txt")) {
 }
 
 #model <<- mx.model.load("./Inception-BN", iteration = 126)
-model <<- mx.model.load("C:/liangjun/mxnet/PreTrainedModel/Inception/Inception-BN", iteration = 126)
+model <<- mx.model.load("C:/liangjun/mxnet/PreTrainedModel/Inception/Inception_BN", iteration = 126)
 
 synsets <<- readLines("synset.txt")
 
